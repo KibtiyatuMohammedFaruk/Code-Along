199c42d8e700ad4f1e0f25aeef26a7300fb6e180
Kibtiyatu Mohammed Faruk <faruk.kibtiya1@gmail.com>
2022-04-04 14:01:01 +0000

    React component 8

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+        <h1>Welcome to our About Page</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, facilis! Est magni ut iusto, consectetur quaerat facere nam, dolor quod eos dolore a numquam dicta itaque incidunt sunt eius nihil.
+        Quam laborum cum facere tempore quo, voluptates expedita cupiditate, sit possimus molestias dolores provident et ipsam magni dolorem commodi hic voluptatem in aliquam maxime deserunt at eveniet odit? At, perspiciatis?
+        Fuga cumque ex praesentium perferendis eligendi quasi consequuntur unde tempore dicta, nihil corporis? Dolores sapiente eligendi fugiat, fuga saepe vel praesentium ipsa quasi, nisi reprehenderit voluptate neque aliquam nulla consequatur?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, facilis! Est magni ut iusto, consectetur quaerat facere nam, dolor quod eos dolore a numquam dicta itaque incidunt sunt eius nihil.
+        Quam laborum cum facere tempore quo, voluptates expedita cupiditate, sit possimus molestias dolores provident et ipsam magni dolorem commodi hic voluptatem in aliquam maxime deserunt at eveniet odit? At, perspiciatis?
+        Fuga cumque ex praesentium perferendis eligendi quasi consequuntur unde tempore dicta, nihil corporis? Dolores sapiente eligendi fugiat, fuga saepe vel praesentium ipsa quasi, nisi reprehenderit voluptate neque aliquam nulla consequatur?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, facilis! Est magni ut iusto, consectetur quaerat facere nam, dolor quod eos dolore a numquam dicta itaque incidunt sunt eius nihil.
+        Quam laborum cum facere tempore quo, voluptates expedita cupiditate, sit possimus molestias dolores provident et ipsam magni dolorem commodi hic voluptatem in aliquam maxime deserunt at eveniet odit? At, perspiciatis?
+        Fuga cumque ex praesentium perferendis eligendi quasi consequuntur unde tempore dicta, nihil corporis? Dolores sapiente eligendi fugiat, fuga saepe vel praesentium ipsa quasi, nisi reprehenderit voluptate neque aliquam nulla consequatur?</p>
+    </section>
+  )
+}
+
+export default About
