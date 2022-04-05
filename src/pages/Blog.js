7bd1df139eaaa6077a8497b4a160dc6d5f2d4502
Kibtiyatu Mohammed Faruk <faruk.kibtiya1@gmail.com>
2022-04-05 14:20:31 +0000

    react component 9

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,20 +1,7 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
 
-const Blog = () => {
-  return (
-    <section>
-        <h1>Welcome to our Blog Page</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, facilis! Est magni ut iusto, consectetur quaerat facere nam, dolor quod eos dolore a numquam dicta itaque incidunt sunt eius nihil.
-        Quam laborum cum facere tempore quo, voluptates expedita cupiditate, sit possimus molestias dolores provident et ipsam magni dolorem commodi hic voluptatem in aliquam maxime deserunt at eveniet odit? At, perspiciatis?
-        Fuga cumque ex praesentium perferendis eligendi quasi consequuntur unde tempore dicta, nihil corporis? Dolores sapiente eligendi fugiat, fuga saepe vel praesentium ipsa quasi, nisi reprehenderit voluptate neque aliquam nulla consequatur?</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, facilis! Est magni ut iusto, consectetur quaerat facere nam, dolor quod eos dolore a numquam dicta itaque incidunt sunt eius nihil.
-        Quam laborum cum facere tempore quo, voluptates expedita cupiditate, sit possimus molestias dolores provident et ipsam magni dolorem commodi hic voluptatem in aliquam maxime deserunt at eveniet odit? At, perspiciatis?
-        Fuga cumque ex praesentium perferendis eligendi quasi consequuntur unde tempore dicta, nihil corporis? Dolores sapiente eligendi fugiat, fuga saepe vel praesentium ipsa quasi, nisi reprehenderit voluptate neque aliquam nulla consequatur?</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, facilis! Est magni ut iusto, consectetur quaerat facere nam, dolor quod eos dolore a numquam dicta itaque incidunt sunt eius nihil.
-        Quam laborum cum facere tempore quo, voluptates expedita cupiditate, sit possimus molestias dolores provident et ipsam magni dolorem commodi hic voluptatem in aliquam maxime deserunt at eveniet odit? At, perspiciatis?
-        Fuga cumque ex praesentium perferendis eligendi quasi consequuntur unde tempore dicta, nihil corporis? Dolores sapiente eligendi fugiat, fuga saepe vel praesentium ipsa quasi, nisi reprehenderit voluptate neque aliquam nulla consequatur?</p>
-    </section>
-  )
+function Blog() {
+  return <Outlet />;
 }
 
-export default Blog
+export default Blog;
